115bb7c1180d7ac26ba050039c26f520284b10b9
koukichi <itikawa2000145385@gmail.com>
2024-06-12 15:57:11 +0900

    fix 404svg

--- src/components/Error404.tsx
+++ src/components/Error404.tsx
@@ -1,4 +1,4 @@
-import ErrImg from './../assets/404.png'
+import ErrImg from './../assets/404.svg'
 
 function Error404(){
     return(
@@ -6,7 +6,7 @@ function Error404(){
             <div className='bg404'>
                 <h1>404 Not Found</h1>
                 <p>お探しのページは見つかりませんでした。</p>
-                <img src={ErrImg} alt='404' />
+                <img src={ErrImg}  alt='404' />
             </div>
         </body>
     )
