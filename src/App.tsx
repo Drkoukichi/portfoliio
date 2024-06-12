816acd62e3cca0978bb8871d623868520ffc9534
koukichi <itikawa2000145385@gmail.com>
2024-06-12 13:25:38 +0900

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom"; // 追加
 import Top from './components/Top.tsx'
 import Ca from './components/CaTech.tsx'
+import Create from './components/Create.tsx'
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Routes> {/*Routesで囲む*/}
         <Route path="/" element={ <Top /> } /> {/*RouteにHomeを設定する*/}
         <Route path="/*" element={ <Top /> } /> {/*RouteにHomeを設定する*/}
+        <Route path="/create" element={ <Create /> } /> {/*RouteにCreateを設定する*/}
         <Route path="/catech" element={ <Ca /> } /> {/*RouteにCaTechを設定する*/}
       </Routes>
       </BrowserRouter>
