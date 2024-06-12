ce37425b11001f5f2a2b66dd70e53ffad0cd9411
koukichi <itikawa2000145385@gmail.com>
2024-06-12 15:46:24 +0900

    add 404 message

--- src/components/Error404.tsx
+++ src/components/Error404.tsx
@@ -2,11 +2,13 @@ import ErrImg from './../assets/404.png'
 
 function Error404(){
     return(
-        <div>
-            <h1>404 Not Found</h1>
-            <p>お探しのページは見つかりませんでした。</p>
-            <img width={800} src={ErrImg} alt="404 Not Found" />
-        </div>
+        <body>
+            <div className='bg404'>
+                <h1>404 Not Found</h1>
+                <p>お探しのページは見つかりませんでした。</p>
+                <img src={ErrImg} alt='404' />
+            </div>
+        </body>
     )
 }
 
