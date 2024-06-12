df0ecf90b40e6d38edf646245be58f2922bd8d72
koukichi <itikawa2000145385@gmail.com>
2024-06-12 15:10:50 +0900

    remove:pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 //import { useState } from 'react'
 import { Routes, Route, BrowserRouter } from "react-router-dom"; // 追加
 import Top from './components/Top.tsx'
-import Ca from './components/CaTech.tsx'
-import Create from './components/Create.tsx'
 import Error from './components/Error404.tsx';
 
 
@@ -14,8 +12,6 @@ function App() {
       <Routes> {/*Routesで囲む*/}
         <Route path="/" element={ <Top /> } /> {/*RouteにHomeを設定する*/}
         <Route path="/self-intro" element={ <Top /> } /> {/*RouteにHomeを設定する*/}
-        <Route path="/create" element={ <Create /> } /> {/*RouteにCreateを設定する*/}
-        <Route path="/catech" element={ <Ca /> } /> {/*RouteにCaTechを設定する*/}
         <Route path="/*" element={<Error />} /> {/*RouteにErrorを設定する*/}
       </Routes>
       </BrowserRouter>
